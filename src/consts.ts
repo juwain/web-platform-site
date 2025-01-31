1a495c937ecafc75ee9fadc29aeb404602f5952f
Zyuzin Vitaly <juwain@yandex.ru>
2025-01-31 13:18:23 +0300

    fix(components): правка текстов

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Журнал «Веб-платформа»';
+export const SITE_TITLE = 'Веб-платформа';
 export const SITE_DESCRIPTION =
-  'Журнал о том, как всё устроено в веб-платформе и что происходит в индустрии фронтенда. Новости, полезные выжимки, находки и напоминания';
+  'О том, как всё устроено в веб-платформе и что происходит в индустрии фронтенда. Новости, полезные выжимки, находки и напоминания';
