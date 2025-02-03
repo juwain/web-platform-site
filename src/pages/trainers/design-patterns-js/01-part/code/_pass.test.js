4b6fe7e7c134383f0450e941bb9e9d218657ee1b
Zyuzin Vitaly <juwain@yandex.ru>
2025-02-03 23:24:04 +0300

    feat(components): разблокирован тренажёр

--- src/pages/trainers/design-patterns-js/01-part/code/_pass.test.js
+++ src/pages/trainers/design-patterns-js/01-part/code/_pass.test.js
@@ -1,23 +1,7 @@
-import { App } from "./index";
-import * as espree from "espree";
+import { App } from './index';
 
-const ast = espree.parse(App, { ecmaVersion: 6, sourceType: "module" });
-
-describe("Задание 1", () => {
-  test("Создана функция App", () => {
-    expect(ast.type).toBe("Program");
-    expect(ast.body[0].type).toBe("FunctionDeclaration");
-    expect(ast.body[0].id.name).toBe("App");
-  });
-
-  test("Объявлена переменная result со значением 'test'", () => {
-    expect(ast.body[0].body.body[0].type).toBe("VariableDeclaration");
-    expect(ast.body[0].body.body[0].declarations[0].id.name).toBe("result");
-    expect(ast.body[0].body.body[0].declarations[0].init.value).toBe("test");
-  });
-
-  test("Функция возвращает переменную result", () => {
-    expect(ast.body[0].body.body[1].type).toBe("ReturnStatement");
-    expect(ast.body[0].body.body[1].argument.name).toBe("result");
+describe('Задание 1', () => {
+  test("Функция App возвращает 'test'", () => {
+    expect(App()).toBe('test');
   });
 });
