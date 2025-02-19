b54da632bd5c0467e2d2839c4361263d90ca80f8
Zyuzin Vitaly <juwain@yandex.ru>
2025-02-19 18:21:50 +0300

    feat(components): добавлена кнопка суппорта

--- src/components/Goals/Goals.tsx
+++ src/components/Goals/Goals.tsx
@@ -1,5 +1,6 @@
-import type { FC } from "react";
-import "./styles.css";
+import type { FC } from 'react';
+import './styles.css';
+import { RoundedButton } from '@codesandbox/sandpack-react';
 
 interface GoalsProps {
   specs?: {
@@ -11,13 +12,23 @@ interface GoalsProps {
 export const Goals: FC<GoalsProps> = ({ specs }) => {
   return (
     <div className="goals">
+      <RoundedButton
+        onClick={() =>
+          window.open(
+            `https://t.me/web_platform_support?text=Hello, world!&profile'`,
+          )
+        }
+        className="need-help"
+      >
+        <span>?</span>
+      </RoundedButton>
       <h2 className="goals-title">Цели:</h2>
       <ul className="goals-list">
         {specs ? (
           specs.map((spec) => (
             <li
               key={spec.name}
-              className={spec.status === "pass" ? "completed" : ""}
+              className={spec.status === 'pass' ? 'completed' : ''}
             >
               {spec.name}
             </li>
