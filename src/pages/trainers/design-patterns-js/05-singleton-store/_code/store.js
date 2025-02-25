825e0b7c6cc5799d8177c7084e516b926b7260ad
Zyuzin Vitaly <juwain@yandex.ru>
2025-02-25 12:42:22 +0300

    fix(content): добавлен jsdoc в 5 часть

--- src/pages/trainers/design-patterns-js/05-singleton-store/_code/store.js
+++ src/pages/trainers/design-patterns-js/05-singleton-store/_code/store.js
@@ -1,8 +1,32 @@
 let instance;
 const data = new Map();
 
+/**
+ * Синглтон-хранилище данных с использованием Map
+ * @class
+ * @example
+ * const store = new SingletonStore();
+ * store.set('key', 'value');
+ * store.get('key'); // 'value'
+ * store.has('key'); // true
+ * store.delete('key'); // true
+ * store.has('key'); // false
+ *
+ * // Все экземпляры разделяют общее хранилище:
+ * const anotherStore = new SingletonStore();
+ * anotherStore.set('num', 42);
+ * store.get('num'); // 42
+ */
 class SingletonStore {
   constructor() {}
+
+  set(key, value) {}
+
+  get(key) {}
+
+  has(key) {}
+
+  delete(key) {}
 }
 
 export default SingletonStore;
