b32e8cca285bd1e876f076fb5f220973e0092c57
Zyuzin Vitaly <juwain@yandex.ru>
2025-03-18 17:02:02 +0300

    fix(components): доработка персиста

--- src/components/utils/usePersistCode.ts
+++ src/components/utils/usePersistCode.ts
@@ -3,22 +3,26 @@ import { useEffect, useLayoutEffect } from 'react';
 import { ls } from '~/utils/localStorage';
 
 export const usePersistCode = () => {
-  const { code, updateCode } = useActiveCode();
+  const { code } = useActiveCode();
   const { sandpack } = useSandpack();
 
-  const key = `${location.pathname}${sandpack.activeFile}`;
-
-  const persistedCode = ls.get(key);
-
   useLayoutEffect(() => {
-    if (persistedCode !== code && persistedCode !== null) {
-      updateCode(persistedCode);
-    }
-  }, [key]);
+    sandpack.visibleFiles.forEach((file) => {
+      const currentFileKey = `${location.pathname}${file}`;
+      const currentPersistedCode = ls.get(currentFileKey);
+
+      if (!currentPersistedCode) return;
+
+      sandpack.updateFile(file, currentPersistedCode, true);
+    });
+  }, [sandpack.visibleFiles]);
 
   useEffect(() => {
-    if (persistedCode !== code) {
-      ls.set(key, code);
+    const currentFileKey = `${location.pathname}${sandpack.activeFile}`;
+    const currentPersistedCode = ls.get(currentFileKey);
+
+    if (currentPersistedCode !== code) {
+      ls.set(currentFileKey, code);
     }
-  }, [code, persistedCode, key]);
+  }, [sandpack.activeFile, code]);
 };
