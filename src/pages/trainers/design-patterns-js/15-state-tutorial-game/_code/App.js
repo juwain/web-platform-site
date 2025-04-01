f7beb8f70d46d1f5fc9ac8fc35ef775463c51e4a
Zyuzin Vitaly <juwain@yandex.ru>
2025-04-01 10:57:15 +0300

    fix(content): рефакторинг разросшихся компонентов

---
+++ src/pages/trainers/design-patterns-js/15-state-tutorial-game/_code/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Игра «Угадай число от 1 до 10»</h1>
+    </div>
+  );
+}
