af8216e2b50a9321abce52a2b7c2eb8a2b4967ce
Zyuzin Vitaly <juwain@yandex.ru>
2025-04-11 23:43:19 +0300

    fix(core): добавляет подсветку синтаксиса

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,23 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://webplatform.tech",
-  integrations: [sitemap(), react()],
-  markdown: {
-    shikiConfig: {
-      // theme: "dracula",
-      themes: {
-        light: "github-light",
-        dark: "github-dark",
+  integrations: [
+    sitemap(),
+    react(),
+    expressiveCode({
+      themes: ["github-dark", "github-light"],
+      styleOverrides: {
+        codeFontFamily: '"PTMonoWeb", monospace',
+        codeFontSize: "1rem",
+        frames: {
+          shadowColor: "transparent",
+        },
       },
-      langs: [],
-      wrap: true,
-      transformers: [],
-    },
-  },
+    }),
+  ],
 });
