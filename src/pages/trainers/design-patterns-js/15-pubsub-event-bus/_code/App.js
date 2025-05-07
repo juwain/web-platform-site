2b1ae2480aa9235de4d4e570125610a95e31cdb6
Zyuzin Vitaly <juwain@yandex.ru>
2025-05-07 18:28:20 +0300

    feat(content): добавлен контент теториала по pubsub

--- src/pages/trainers/design-patterns-js/15-pubsub-event-bus/_code/App.js
+++ src/pages/trainers/design-patterns-js/15-pubsub-event-bus/_code/App.js
@@ -1,26 +1,14 @@
-import { Notifications } from './Notifications';
-import { emit } from './useEvent';
 import './style.css';
 
 export default function App() {
-  const handleClick = (type) => {
-    emit('notify', {
-      type,
-      message: `Событие: ${type} - ${new Date().toLocaleTimeString()}`,
-    });
-  };
-
   return (
     <>
-      <Notifications />
-
       <div className="container">
         <h1>Система уведомлений</h1>
 
         <div className="controls">
-          <button onClick={() => handleClick('error')}>Показать ошибку</button>
-          <button onClick={() => handleClick('success')}>Показать успех</button>
-          <button onClick={() => handleClick()}>Показать хз</button>
+          <button>Показать ошибку</button>
+          <button>Показать успех</button>
         </div>
       </div>
     </>
---
+++ src/pages/trainers/design-patterns-js/15-pubsub-event-bus/_code/_App.js
@@ -0,0 +1,27 @@
+import { Notifications } from './Notifications';
+import { emit } from './useEvent';
+import './style.css';
+
+export default function App() {
+  const handleClick = (type) => {
+    emit('notify', {
+      type,
+      message: `Событие: ${type}`,
+    });
+  };
+
+  return (
+    <>
+      <Notifications />
+
+      <div className="container">
+        <h1>Система уведомлений</h1>
+
+        <div className="controls">
+          <button onClick={() => handleClick('error')}>Показать ошибку</button>
+          <button onClick={() => handleClick('success')}>Показать успех</button>
+        </div>
+      </div>
+    </>
+  );
+}
